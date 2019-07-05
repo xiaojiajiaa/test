44421f3e652f80ca1883e6d53718b5f254d9cc61
xiaojiajiaa <1255075959@qq.com>
2019-07-06 01:16:18 +0800

    首次提交

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    asdsda
 }
 
 MainWindow::~MainWindow()
